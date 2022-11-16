09f6e4a4391268316aa74fa0eaf5d38c8e5ac6c0
apollinariia <apollinariia.orlova@gmail.com>
2022-11-16 14:44:29 +0300

    fix wand following logic

--- MagicGP/Assets/Scripts/MouseTrack.cs
+++ MagicGP/Assets/Scripts/MouseTrack.cs
@@ -16,11 +16,14 @@ public class MouseTrack : MonoBehaviour
     private void Update()
     {
         Vector3 screenMousePosition = Input.mousePosition;
-        //Vector3 pos = new Vector3 (0f, 260f, 15f);
 
+        // если мы не определяем корректно глубину, для нуля действительно актуальной будет позиция камеры,
+        // так как она соответвует camera frustrum в этой позиции, обсудим на паре.
+        // Чтобы это исправить достаточно использовать глубину камеры для z
+        screenMousePosition.z = _camera.depth * 5;
         Vector3 worldMousePosition = _camera.ScreenToWorldPoint(screenMousePosition);
-        Debug.Log(worldMousePosition);
+        Debug.Log($"screenMousePosition - {screenMousePosition}, worldMousePosition - {worldMousePosition}");
         
-        transform.LookAt(-screenMousePosition);
+        transform.LookAt(-worldMousePosition);
     }
 }
