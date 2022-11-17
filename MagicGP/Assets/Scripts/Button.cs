8de81d1d4c9c878ddeca22a8e1ec0b50f5a2cef9
dariakrtn <dariakorotinawork@yandex.ru>
2022-11-17 21:45:23 +0300

    работа кнопки и исправлена канва

---
+++ MagicGP/Assets/Scripts/Button.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void ButtonPlay()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void ButtonBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ButtonSetting()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
